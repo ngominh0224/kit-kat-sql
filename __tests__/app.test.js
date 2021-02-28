bc640a3b9f7552fa54a64bb83e58091b09542dde
Minh Ngo <ngo.minh0224@gmail.com>
2021-02-28 02:22:16 -0800

    put and delete tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -154,5 +154,63 @@ test('creates new kit-kat and new kit-kat is in our list', async () => {
 
       expect(strawberryKitKat).toEqual(expectedKitKat);
     });
+
+    test('updates a kit-kat bar', async () => {
+      const newKitKat = {
+        name: 'Banana Kit-Kat',
+        description: 'Banana chocolate wafers',
+        category: 'unique',
+        is_flavored: true,
+        size: 'regular',
+        price: 1,
+      };
+
+      const expectedKitKat = {
+        ...newKitKat,
+        id: 2,
+        owner_id: 1,
+      };
+
+     await fakeRequest(app)
+        .put('/kitkats/2')
+        .send(newKitKat)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      const updateKitKats = await fakeRequest(app)
+        .get('/kitkats/2')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(updateKitKats.body).toEqual(expectedKitKat);
+    });
+
+test('deletes a kit-kat with matching id', async () => {
+      const expectation = {
+        id: 3,
+        name: 'Dark-Kit-Kat',
+        description: 'Dark Chocolate Wafers',
+        category: 'unique',
+        is_flavored: true,
+        size: 'Regular',
+        price: 1,
+        owner_id: 1,
+      };
+
+
+     const data = await fakeRequest(app)
+        .delete('/kitkats/3')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+      
+      const nothing = await fakeRequest(app)
+        .get('/kitkats/3')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(nothing.body).toEqual('');
+    });
   });
 });
